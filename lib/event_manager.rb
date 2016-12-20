98e0f71b06f13368c700412c6a7d646db9706bc1
Raiko <raiko4238@gmail.com>
2016-12-21 00:21:41 +0200

    finished I: Time Targeting

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -42,14 +42,14 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 template_letter = File.read 'form_letter.erb'
 erb_template = ERB.new template_letter
 
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
-  form_letter = erb_template.result(binding)
-  save_thank_you_letters(id, form_letter)
-end
+#contents.each do |row|
+#  id = row[0]
+#  name = row[:first_name]
+#  zipcode = clean_zipcode(row[:zipcode])
+#  legislators = legislators_by_zipcode(zipcode)
+#  form_letter = erb_template.result(binding)
+#  save_thank_you_letters(id, form_letter)
+#end
 
 # Display all names and phone numbers.
 def display_phone_numbers
@@ -61,4 +61,19 @@ def display_phone_numbers
   end
 end
 
+def time_targeting
+  contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+  hours = Hash.new(0)
+  contents.each do |row|
+    reg_date_string = row[:regdate]
+    reg_date = DateTime.strptime(reg_date_string, '%m/%d/%y %H:%M')
+    hours[reg_date.hour] += 1
+  end
+  hours.sort_by {|k, v| v}.reverse.each do |hour, regs|
+    puts "Hour: #{hour}, registered: #{regs}"
+  end
+end
+
+
 display_phone_numbers
+time_targeting
